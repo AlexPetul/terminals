4d6ad26bfd8f81295eb2cc509587cd8128d6f8a7
Petul Alexey <a.petul@icode.by>
2022-06-14 20:29:31 +0300

    Add ci/cd

--- app/accounts/tests/test_registration.py
+++ app/accounts/tests/test_registration.py
@@ -20,6 +20,10 @@ def api_client():
     ],
 )
 def test_weak_passwords(api_client: APIClient, password: str):
+    """
+    Test different kind of weak passwords. For example with missing upper or lower case letter,
+    length less than 14 etc...Passwords should be complied with CIS requirements.
+    """
     response = api_client.post(reverse("api-users-list"), {"email": "somerandom@email.com", "password": password})
 
     assert response.status_code == status.HTTP_400_BAD_REQUEST
